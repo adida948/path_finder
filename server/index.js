f59739e6fe707e3f23117f246abca7f90727c210
Terrance Xin <xin.terrance@gmail.com>
2018-09-01 17:36:19 -0400

    Refactor server side

--- server/index.js
+++ server/index.js
@@ -5,20 +5,10 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-const mongoose = require('mongoose');
 const router = require('./router');
-const { seedTheTalks, seedMaria } = require('./services/seeds');
 
 module.exports = { io };
 const socketManager = require('./services/socketManager');
-
-const mongodbServer = process.env.MONGO_AMEXMEETS || 'mongodb://localhost:27017/amexmeets';
-mongoose.connect(mongodbServer, () => {
-  // mongoose.connection.db.dropDatabase();
-  seedTheTalks();
-  seedMaria();
-});
-mongoose.set('debug', true);
 
 app.use(bodyParser.json({ limit: '5mb' }));
 app.use(bodyParser.urlencoded({ extended: true }));
--- server/services/socketManager.js
+++ server/services/socketManager.js
@@ -1,80 +1,10 @@
 const { io } = require('../index');
-const { disconnectUser } = require('../utils');
-const {
-  LOGOUT,
-  MESSAGE_SENT,
-  MESSAGE_SENT_PRIVATE,
-  STOPPED_TYPING,
-  STOPPED_TYPING_PRIVATE,
-  TYPING,
-  TYPING_PRIVATE,
-  USER_CONNECTED,
-  PING_PONG,
-  END_POS,
-} = require('../../constants');
-const UserModel = require('../models/user');
+const { PING_PONG, END_POS } = require('../../constants');
 
 module.exports = (socket) => {
-  socket.on(USER_CONNECTED, (user) => {
-    const { username } = user;
-    console.log(`${username} connected`);
-
-    UserModel.find({}, (err, users) => {
-      if (err) {
-        return console.log(`socket connected fetch all users failed: ${err}`);
-      }
-
-      const updatedUsers = users.map((_user) => {
-        const { id, onlineStatus } = _user;
-
-        return ({ id, username: _user.username, onlineStatus });
-      });
-
-      io.emit(USER_CONNECTED, {
-        users: updatedUsers,
-        notice: `${username} connected`,
-      });
-    });
-
-    socket.on('disconnect', () => {
-      console.log(`${username} has disconnected`);
-      disconnectUser(socket, user);
-    });
-  });
-
-  socket.on(LOGOUT, (user) => {
-    console.log(`${user.username} has disconnected`);
-    disconnectUser(socket, user);
-  });
-
-  socket.on(MESSAGE_SENT, (data) => {
-    io.emit(MESSAGE_SENT, data);
-  });
-
-  socket.on(MESSAGE_SENT_PRIVATE, (data) => {
-    io.emit(MESSAGE_SENT_PRIVATE, data);
-  });
-
-  socket.on(TYPING, (username) => {
-    socket.broadcast.emit(TYPING, username);
-  });
-
-  socket.on(STOPPED_TYPING, (username) => {
-    io.emit(STOPPED_TYPING, username);
-  });
-
-  socket.on(TYPING_PRIVATE, (username) => {
-    socket.broadcast.emit(TYPING_PRIVATE, username);
-  });
-
-  socket.on(STOPPED_TYPING_PRIVATE, (username) => {
-    io.emit(STOPPED_TYPING_PRIVATE, username);
-  });
-
   socket.on(END_POS, (obj) => {
     io.emit(END_POS, obj);
   });
 
-  socket.on(PING_PONG, () => {
-  });
+  socket.on(PING_PONG, () => {});
 };
