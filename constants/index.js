d3f92dbd287c182950cdea994c2508fd5d85a9e5
Terrance Xin <xin.terrance@gmail.com>
2018-09-03 17:23:51 -0400

    Update constants

--- constants/index.js
+++ constants/index.js
@@ -1,12 +1,10 @@
 module.exports = {
-  FLOOR: 'FLOOR',
-  TOGGLE_END_POS: 'TOGGLE_END_POS',
-  END_POS: 'END_POS',
+  SELECT_FLOOR: 'SELECT_FLOOR',
+  UPDATE_GRID: 'UPDATE_GRID',
   SOCKET_EVENTS: [
-    'END_POS',
+    'UPDATE_GRID',
   ],
   PING_PONG: 'PING_PONG',
   rootUrl: () => process.env.ROOT_URL || 'http://localhost:3000',
-  TOGGLE_APP: 'TOGGLE_APP',
 };
 
