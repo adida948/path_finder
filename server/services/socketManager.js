03249875c32ec21402d7cbfa2946fa8fd2b6b9d5
Terrance Xin <xin.terrance@gmail.com>
2018-09-03 17:24:15 -0400

    Update sockets

--- server/services/socketManager.js
+++ server/services/socketManager.js
@@ -1,9 +1,9 @@
 const { io } = require('../index');
-const { PING_PONG, END_POS } = require('../../constants');
+const { PING_PONG, UPDATE_GRID } = require('../../constants');
 
 module.exports = (socket) => {
-  socket.on(END_POS, (obj) => {
-    io.emit(END_POS, obj);
+  socket.on(UPDATE_GRID, (obj) => {
+    io.emit(UPDATE_GRID, obj);
   });
 
   socket.on(PING_PONG, () => {});
