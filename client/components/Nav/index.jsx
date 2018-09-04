c4e92d113fdad2d756d1b3c18a9ae0bd368ffd72
Terrance Xin <xin.terrance@gmail.com>
2018-09-04 14:05:12 -0400

    Change header to nav

---
+++ client/components/Nav/index.jsx
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+import { selectFloor } from '../../actions/grid';
+
+class Nav extends Component {
+  constructor(props) {
+    super(props);
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(floor) {
+    return () => {
+      this.props.selectFloor(floor);
+    };
+  }
+
+  render() {
+    return (
+      <div className="nav">
+        <button
+          className="nav__btn--cafe"
+          onClick={this.handleClick('cafe')}
+        >Restroom
+        </button>
+
+        <button
+          className="nav__btn--floor"
+          onClick={this.handleClick('floor')}
+        >Snack Room
+        </button>
+
+        <button
+          className="nav__btn--aud"
+          onClick={this.handleClick('aud')}
+        >Table Tennis
+        </button>
+      </div>
+    );
+  }
+}
+
+Nav.propTypes = {
+  selectFloor: PropTypes.func.isRequired,
+};
+
+export default connect(
+  null,
+  {
+    selectFloor,
+  },
+)(Nav);
